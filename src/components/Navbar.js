a8da38c08801a2a8a16360321f5a43d68b782053
MurtazaBagwala07 <murtazabag786@gmail.com>
2021-12-04 17:18:22 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <a>Home</a>
+            <a>PokeDex</a>
+            <a>Fight</a>
+        </nav>
+    )
+}
+
+export default Navbar
