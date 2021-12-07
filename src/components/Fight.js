97fdb63555e499f148ed97366ff7cad01d00657c
MurtazaBagwala07 <murtazabag786@gmail.com>
2021-12-08 00:18:55 +0530

    fight page done

--- src/components/Fight.js
+++ src/components/Fight.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import PokemonTypes from '../assets/PokemonTypes.png'
+import FightElements from '../assets/FightElements.jpg'
 
 const Fight = () => {
     return (
-        <div>
-            Fight
+        <div className="fight-wrapper">
+            <div className="elements">
+                <div className="elements-heading">
+                    Elements in Pokemon
+                </div>
+                <div className="elements-logo">
+                    <img src={PokemonTypes} alt="" />
+                </div>
+            </div>
+            <div className="fight-elements">
+                <div className="fight-elements-heading">
+                    What elements to use against different elements
+                </div>
+                <div className="fight-elements-logo">
+                    <img src={FightElements} alt="" />
+                </div>
+            </div>
+            <div className="abbreviations">
+                <p>SE : Super Effective</p>
+                <p>Reg : Regular</p>
+                <p>NVE : Not Very Effective</p>
+            </div>            
         </div>
     )
 }
